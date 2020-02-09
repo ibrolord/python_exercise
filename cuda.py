4ef628d450b29ca96734abd8a273d14e553654c8
bagunbiade <bagunbiade@opentext.com>
2020-02-08 22:29:35 -0500

    Updated

---
+++ cuda.py
@@ -0,0 +1,22 @@
+import cli
+
+@cli.command()
+def cuda_operation():
+    """Performs Vectorized Operations on GPU"""
+
+    x = real_estate_array()
+    y = real_estate_array()
+
+    print("Moving calculations to GPU memory")
+    x_device = cuda.to_device(x)
+    y_device = cuda.to_device(y)
+    out_device = cuda.device_array(shape=(x_device.shape[0], x_device.shape[1]), dtype=np.float32)
+    print(x_device)
+    print(x_device.shape)
+    print(x_device.dtype)
+
+    print("Calculating on GPU")
+    add_ufunc(x_device, y_device, out=out_device)
+
+    out_host = out_device.copy_to_host()
+    print(f"Calculations from GPU {out_host}")
---
+++ timing.py
@@ -0,0 +1,19 @@
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        ts = time()
+        result = f(*args, **kwargs)
+        te = time()
+        print(f"fun: {f.__name__}, args: [{args}, {kwargs}] took: {te-ts} sec")
+        return result
+    return wrap
+
+@timing
+@numba.jit(nopython=True)
+def expmean_jit(rea):
+    """Perform multiple mean calculations"""
+
+    val = rea.mean() ** 2
+    return val
+
+
